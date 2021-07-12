35923012c6b6e2721d51523e6a2bb2902363c4ca
IZanko <zankoiva@hotmail.com>
2021-07-12 14:14:18 -0400

    Styling added, display pokemon in document

--- js/script.js
+++ js/script.js
@@ -3,4 +3,14 @@ pokemonList[0] = {name:'Pikachu', height:0.4, type:['electric']};
 pokemonList[1] = {name:'Charizard', height:1.7, type:['fire', 'flying']};
 pokemonList[2] = {name:'Vileplume', height:1.2, type:['grass', 'poison']};
 
-console.log(pokemonList);
+/*display each pokemon in the array including their height. If height > 1.5, 
+add special note of how big it is*/
+document.write('<br>');
+for (var i = 0; i < pokemonList.length; i++) {
+	if (pokemonList[i].height>1.5) {
+		document.write(pokemonList[i].name+' (height: '+pokemonList[i].height+' m) '+'- Wow, thats BIG!'+'<br>');
+	} else {
+		document.write(pokemonList[i].name+' (height: '+pokemonList[i].height+' m) '+'<br>');
+	}
+}
+

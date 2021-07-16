c55457ae3e7a6de7bf309d8b2839c579c0137002
IZanko <zankoiva@hotmail.com>
2021-07-16 09:56:10 -0400

    create function addListItem, update forEach loop

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
 //initiate the array of pokemon protected from global  access
-let pokemonRepository = (function() {
+let pokemonRepository = (function () {
 	let pokemonList = [];
-	pokemonList[0] = {name:'Pikachu', height:0.4, type:['electric']};
-	pokemonList[1] = {name:'Charizard', height:1.7, type:['fire', 'flying']};
-	pokemonList[2] = {name:'Vileplume', height:1.2, type:['grass', 'poison']};
+	pokemonList[0] = { name: "Pikachu", height: 0.4, type: ["electric"] };
+	pokemonList[1] = { name: "Charizard", height: 1.7, type: ["fire", "flying"] };
+	pokemonList[2] = { name: "Vileplume",height: 1.2,type: ["grass", "poison"] };
 
-	function add(pokemon) { pokemonList.push(pokemon);}
-	function getAll() {return pokemonList}
+	function add(pokemon) {
+		pokemonList.push(pokemon);
+	}
+	function getAll() {
+		return pokemonList;
+	}
+	function addListItem(pokemon) {
+		let pokemonList = document.querySelector('.pokemon-list');
+		let listItem = document.createElement('li');
+		let button = document.createElement('button');
+		button.innerText = pokemon.name;
+		button.classList.add('pokemon-button'); //add class pokemon-button
+		pokemonList.appendChild(listItem);
+		listItem.appendChild(button);
+	}
 
 	return {
 		add: add,
-		getAll: getAll
-	}
+		getAll: getAll,
+		addListItem: addListItem,
+	};
 })();
 
-/*display each pokemon in the array including their height. If height > 1.5, 
-add special note of how big it is*/
-document.write('<br>');
-pokemonRepository.getAll().forEach(function(pokemon) {
-	if (pokemon.height > 1.5) {document.write('<p>'+pokemon.name+' (height: '+pokemon.height+' m) '+'- Wow, thats BIG!<p>')}
-	else {document.write('<p>'+pokemon.name+' (height: '+pokemon.height+' m) <p>');}
-})
 
-
-
+document.write("<br>");
+pokemonRepository.getAll().forEach(function (pokemon) {
+	pokemonRepository.addListItem(pokemon);	//create a list item and button for each pokemon on list
+});
